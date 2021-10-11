1dd8a6a0d266fc651722140684472b9cd6464d1e
jad714 <josh01100100@gmail.com>
2021-10-11 16:01:22 -0400

    Added Exercise 41 Project Skeleton

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution41 {
+
+    private String[] fileIO(String filename){
+        // Scans file for names and return an array of strings containing the names to be sorted. Return the String array.
+    }
+
+    public String[] sortNames(String[] names){
+        // Sort the names in the passed list alphabetically and return the alphabetized list (first calls the fileIO method). Return.
+    }
+
+    public static void main(String[] args){
+        // Call sortNames to scan the files and sort the names alphabetically.  Print them in formatted fashion.
+    }
+}
